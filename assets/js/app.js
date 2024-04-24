6059dfae0e69488335358added03a5ab85ce3d1b
tabingtons <harry.tavis.brown@icloud.com>
2024-04-25 06:58:08 +1200

    Finalisation of nav incl. mobile responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,15 @@
 // Nav hamburgerburger selections
 
 const burger = document.querySelector("#burger-menu");
+const menu = document.querySelector("#menu");
+const cross = document.querySelector("#cross");
 const ul = document.querySelector("nav ul");
 const nav = document.querySelector("nav");
 
 burger.addEventListener("click", () => {
     ul.classList.toggle("show");
+    menu.classList.toggle("show");
+    cross.classList.toggle("show");
 });
 
 // Close hamburger menu when a link is clicked
